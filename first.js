d7fec1dccf2822137edc2ae14482417444d70186
Mona1612 <147960336+Mona1612@users.noreply.github.com>
2023-10-16 15:08:48 +0530

    Update first.js

--- first.js
+++ first.js
@@ -32,3 +32,17 @@ logoutButton.addEventListener('click', () => {
     // Redirect or update the UI as needed
   });
 });
+// JavaScript for adding tasks
+function addTask() {
+    const taskInput = document.getElementById('task');
+    const taskText = taskInput.value.trim();
+
+    if (taskText) {
+        const taskList = document.getElementById('taskList');
+        const taskItem = document.createElement('li');
+        taskItem.textContent = taskText;
+        taskList.appendChild(taskItem);
+        taskInput.value = ''; // Clear the input field
+    }
+}
+
