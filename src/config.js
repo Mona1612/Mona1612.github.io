e7f31425b9cf75b04c63610f3560594f13746253
Mona1612 <147960336+Mona1612@users.noreply.github.com>
2023-10-17 20:20:45 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,15 +1,15 @@
 export default {
     oidc: {
-      issuer: 'https://dev-58683679.okta.com/oauth2/default',
-      clientId: '0oacsesyogSKHU8W95d7',
+      issuer: 'https://dev-18400082.okta.com/oauth2/default',
+      clientId: '0oacszhuupfK11QXm5d7',
       scopes: ['openid', 'profile', 'email'],
       redirectUri: `${window.location.origin}/login/callback`,
       useInteractionCodeFlow: false
 
     },
     widget: {
-      issuer: 'https://dev-58683679.okta.com/oauth2/default',
-      clientId: '0oacsesyogSKHU8W95d7',
+      issuer: 'https://dev-18400082.okta.com/oauth2/default',
+      clientId: '0oacszhuupfK11QXm5d7',
       redirectUri: `${window.location.origin}/login/callback`,
       scopes: ['openid', 'profile', 'email'],
       useInteractionCodeFlow: false
